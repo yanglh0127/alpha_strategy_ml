31a2150b11bb7acf186f00523815eec5c10ffe82
LihaiYang <1924287077@qq.com>
2021-04-20 16:58:08 +0800

    按因子逻辑组合单因子

--- fac_expand_from_others.py
+++ fac_expand_from_others.py
@@ -4,6 +4,8 @@ import json
 from utils_func import query_data
 from ft_platform.factor_process import fetch
 import time
+from ft_platform.utils import utils_calculation as uc
+import pickle
 
 data_pat = 'E:/FT_Users/LihaiYang/Files/factor_comb_data/fac_meaning/all_cluster'
 
@@ -43,6 +45,65 @@ factor_value = fetch.fetch_factor(begin, end, fields=other_fac_all, standard='cl
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 # 删除因子数据不足的因子
 factor_value = {k: v for k, v in factor_value.items() if len(v) == len(query_data.get_trade_days('d', from_trade_day=begin, to_trade_day=end))}
+f = open(data_pat + '/fac_expand/other_fac_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value, f, -1)
+f.close()
 
 # 调整因子的正负
+factor_value_adj = {}
+for fac in factor_value.keys():
+    fac_info = query_data.get_alphafactors_info(factor_name=fac)[0]
+    if fac_info['user'] in ['wangfeng', 'JiahuLi', 'Alex']:
+        factor_value_adj[fac] = factor_value[fac] * uc.sign(fac_info['perf'][list(fac_info['perf'].keys())[0]]['ic-mean'])
+    if fac_info['user'] == 'yyzhao':
+        factor_value_adj[fac] = factor_value[fac] * uc.sign(fac_info['perf'][list(fac_info['perf'].keys())[0]]['IC'])
+    if fac_info['user'] == 'cshu':  # 舒畅的因子正负调整?
+        factor_value_adj[fac] = factor_value[fac] * fac_info['perf_summary']['2018-03-09__2021-03-08']['1_d']['sign']
 
+# 拆解
+new_name = list(factor_value_adj.keys())
+factor_value_adj1 = {k: factor_value_adj[k] for k in new_name[0:100]}
+factor_value_adj2 = {k: factor_value_adj[k] for k in new_name[100:200]}
+factor_value_adj3 = {k: factor_value_adj[k] for k in new_name[200:300]}
+factor_value_adj4 = {k: factor_value_adj[k] for k in new_name[300:400]}
+factor_value_adj5 = {k: factor_value_adj[k] for k in new_name[400:500]}
+factor_value_adj6 = {k: factor_value_adj[k] for k in new_name[500:537]}
+
+f = open(data_pat + '/fac_expand/other_facadj_1_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj1, f, -1)
+f.close()
+f = open(data_pat + '/fac_expand/other_facadj_2_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj2, f, -1)
+f.close()
+f = open(data_pat + '/fac_expand/other_facadj_3_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj3, f, -1)
+f.close()
+f = open(data_pat + '/fac_expand/other_facadj_4_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj4, f, -1)
+f.close()
+f = open(data_pat + '/fac_expand/other_facadj_5_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj5, f, -1)
+f.close()
+f = open(data_pat + '/fac_expand/other_facadj_6_20170101-20210228.pkl', 'wb')
+pickle.dump(factor_value_adj6, f, -1)
+f.close()
+
+"""
+# 对other_fac_all中的每个因子，将其划入到与自己的某类因子相关性最高的那类中
+mine_data = pd.read_pickle(data_pat + '/fac_last.pkl')
+all_data = dict(mine_data, **factor_value)
+all_data = {k: v[(v.index >= '2017-01-01') & (v.index <= '2020-08-31')] for k, v in all_data.items()}
+# 基础函数，计算因子之间的相关系数
+def cal_factor_corr(fac_dict, pat_str):
+    if not os.path.exists(pat_str):  # 判断是否存在文件夹如果不存在则创建为文件夹
+        os.makedirs(pat_str)
+    total_data = pd.concat(fac_dict.values(), keys=fac_dict.keys())
+    total_data = total_data.reset_index().set_index('level_1')
+    corank_total = total_data.groupby(total_data.index).apply(lambda g: g.set_index('level_0').T.corr('spearman'))
+    co_rank = corank_total.groupby(corank_total.index.get_level_values(1)).mean()
+    co_rank = co_rank.reindex(co_rank.columns)  # 调整顺序，化为对称阵
+    co_rank.to_csv(pat_str + "/rank_corr.csv", index=True, encoding='utf_8_sig')
+    return co_rank
+
+cal_factor_corr(all_data, data_pat + '/fac_expand')
+"""
