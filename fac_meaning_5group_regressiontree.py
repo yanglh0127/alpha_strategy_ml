c407fa3fbc8bac87338ff1d7cd63caeeccb2397d
LihaiYang <1924287077@qq.com>
2021-07-12 09:53:18 +0800

    树模型组合五类因子

---
+++ fac_meaning_5group_regressiontree.py
@@ -0,0 +1,58 @@
+from ft_platform import fetch_data
+from ft_platform.utils import utils_calculation as uc
+import pandas as pd
+import pickle
+from utils_func import query_data
+from ft_platform.factor_process import fetch
+from copy import deepcopy
+import numpy as np
+import time
+import json
+from matplotlib import pyplot as plt
+import statsmodels.api as sm
+from sklearn.tree import DecisionTreeRegressor
+
+data_pat = 'E:/FT_Users/LihaiYang/Files/factor_comb_data/fac_meaning/5group/linear_model'  # 记得修改
+
+begin = '2015-01-01'
+end = '2021-03-31'  # 记得修改
+trade_days = query_data.get_trade_days('d', from_trade_day=begin, to_trade_day=end)
+
+new_f = pd.read_pickle(data_pat + '/new_f.pkl')
+new_f = new_f.dropna(how='any')  # 所有因子值都不为空
+
+depth_m = 5
+
+def pool_tree_pred(ro_wind, pre_wind):
+    prediction = {}
+    coef_param = {}
+    for i in np.arange((ro_wind + pre_wind), len(trade_days), 1):
+        # 截取样本区间pool在一起计算回归系数
+        date_roll = pd.to_datetime(trade_days[(i - ro_wind - pre_wind):(i - pre_wind)])
+        sub_data = new_f.loc[date_roll, :]
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+        tr = DecisionTreeRegressor(max_depth=depth_m).fit(sub_data.iloc[:, 0:-1], sub_data.iloc[:, -1])
+        coef_param[trade_days[i]] = pd.Series(tr.feature_importances_, index=sub_data.iloc[:, 0:-1].columns)  # 保留参数
+        print("correct rate: ", tr.score(sub_data.iloc[:, 0:-1], sub_data.iloc[:, -1]))
+
+        test_data = new_f.loc[pd.to_datetime(trade_days[i]), :]  # 参数隔（pred_window+1）天后才能用
+        test_data = test_data.drop(['stock_rela'], axis=1)
+        prediction[trade_days[i]] = pd.Series(tr.predict(test_data), index=test_data.index)
+        print(trade_days[i])
+    pred = pd.concat(prediction, axis=1).T
+    pred.index = pd.to_datetime(pred.index)
+    cof = pd.concat(coef_param, axis=1).T
+    cof.index = pd.to_datetime(cof.index)
+    return pred, cof
+
+pred_result = {}
+coef_result = {}
+pred_result['pool_480_' + str(depth_m)], coef_result['pool_480_' + str(depth_m)] = pool_tree_pred(480, 10)
+
+f = open(data_pat + '/regression_tree/fac_' + str(depth_m) + '.pkl', 'wb')  # 记得修改
+pickle.dump(pred_result, f, -1)
+f.close()
+f = open(data_pat + '/regression_tree/coef_' + str(depth_m) + '.pkl', 'wb')  # 记得修改
+pickle.dump(coef_result, f, -1)
+f.close()
