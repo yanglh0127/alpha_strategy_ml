b2663b1da9115b59b23f62527b1b8ca4069209e9
LihaiYang <1924287077@qq.com>
2021-07-21 16:49:13 +0800

    神经网络组合五类因子

--- strategy.py
+++ strategy.py
@@ -8,9 +8,9 @@ import time
 
 begin_date = '2015-01-01'  # 记得修改
 end_date = '2019-12-31'  # 记得修改
-data_pat = 'E:/FT_Users/LihaiYang/Files/factor_comb_data/fac_meaning/5group/linear_model/random_forest'  # 这边路径记得改
+data_pat = 'E:/FT_Users/LihaiYang/Files/factor_comb_data/fac_meaning/5group/linear_model/neural_network'  # 这边路径记得改
 pm_pat = 'eq_tvwap'  # 记得修改
-fac_data = pd.read_pickle(data_pat + '/fac_100_20_0.6.pkl')  # 记得修改
+fac_data = pd.read_pickle(data_pat + '/fac_(16, 8, 4, 2)_256_0.001.pkl')  # 记得修改
 
 
 # 组合权重设置（一）：使用优化函数
